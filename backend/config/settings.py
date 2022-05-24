8d5b6b43d5e48f4c7578a38768e513f1ada31dfb
forza111 <nikitos.ionkin@yandex.ru>
2022-05-25 02:52:06 +0300

    Добавление команды выполнения миграций при запуске контейнера.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.postgresql"),
-        "NAME": "postgres",
+        "NAME": os.environ.get("POSTGRES_DB", "postgres"),
         "USER": os.environ.get("POSTGRES_USER", "postgres"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
         "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
